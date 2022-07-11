362166bee0371644b371bacf000392163394688f
Pasakorn Tiwatthanont <ptiwatthanont@gmail.com>
2022-07-11 11:13:52 +0700

    Init

---
+++ inaccurate_gps_robotics.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class GPS {
+
+};
+
+class Robot {
+
+};
+
+class Simulation {
+    
+};
+
+int main() {
+    cout << "hello world" << endl;
+    return 0;
+}
