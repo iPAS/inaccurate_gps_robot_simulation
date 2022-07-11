6f6708d92d18540c946782906bb70fea91d52b25
Pasakorn Tiwatthanont <ptiwatthanont@gmail.com>
2022-07-11 15:21:30 +0700

    Robot class

--- robot.cpp
+++ robot.cpp
@@ -1,1 +1,18 @@
 #include "robot.h"
+
+Robot::Robot(Location &init_loc, int step_size = 1) : gps(init_loc) {
+    this->step_size = step_size;
+}
+
+void Robot::do_command(command_t cmd) {
+    switch (cmd) {
+    case GO_NORTH:
+        break;
+    case GO_EAST:
+        break;
+    case GO_SOUTH:
+        break;
+    case GO_WEST:
+        break;
+    };
+}
--- robot.h
+++ robot.h
@@ -1,8 +1,24 @@
 #pragma once
 
+#include "gps.h"
+
 #include <iostream>
 using namespace std;
 
-class Robot {
+typedef enum {
+    GO_NORTH,
+    GO_EAST,
+    GO_SOUTH,
+    GO_WEST
+} command_t;
 
+class Robot {
+    private:
+        GPS gps;
+        int step_size;
+
+    public:
+        Robot(Location &, int);
+
+        void do_command(command_t);
 };
