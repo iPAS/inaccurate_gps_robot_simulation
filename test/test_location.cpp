8d14dadcd26adfa79d90dce17aaa62c477ecc1f4
Pasakorn Tiwatthanont <ptiwatthanont@gmail.com>
2022-07-20 15:23:51 +0700

    Complete test for the Location class.

--- test/test_location.cpp
+++ test/test_location.cpp
@@ -19,17 +19,30 @@ void tearDown(void) {
 }
 
 void test_operator_type_casting(void) {
+    TEST_ASSERT_EQUAL_STRING_MESSAGE("(0, 0)", *pLoc, "Type casting of Location object wrong!");
 }
 
 void test_operator_equal(void) {
+    Location l1(0, 0);
+    Location l2(0, 1);
+    string str;
+    
+    TEST_ASSERT_TRUE(*pLoc == l1);
+    TEST_ASSERT_FALSE(*pLoc == l2);
 }
 
 void test_operator_self_addition(void) {
-
+    *pLoc += Location(1, 1);
+    TEST_ASSERT_TRUE(*pLoc == Location(1, 1));
+    *pLoc += Location(-10, 5);
+    TEST_ASSERT_TRUE(*pLoc == Location(-9, 6));
+    *pLoc += Location(9, -6);
+    TEST_ASSERT_TRUE(*pLoc == Location(0, 0));
 }
 
 void test_operator_self_assignment(void) {
-
+    *pLoc = Location(100, -100);
+    TEST_ASSERT_TRUE(*pLoc == Location(100, -100));
 }
 
 int main(void) {
